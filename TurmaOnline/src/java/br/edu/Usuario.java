478436ff6f5e9db6aa74806695e309be5a2da022
marciusbrandao <marciusbrandao@52e366fe-165d-cc53-b111-9e97750322fe>
2014-08-05 23:26:10 +0000

    novo cadastro de usuarios e envio de email.

--- TurmaOnline/src/java/br/edu/Usuario.java
+++ TurmaOnline/src/java/br/edu/Usuario.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.edu;
 
+import br.edu.util.GMailBuilder;
 import entities.Context;
 import entities.Repository;
+import entities.annotations.ActionDescriptor;
 import entities.annotations.Param;
 import entities.annotations.PropertyDescriptor;
 import entities.annotations.UserRoles;
@@ -29,238 +25,193 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.apache.commons.lang.RandomStringUtils;
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.NotEmpty;
+import util.jsf.Types;
 
 /**
  *
  * @author Vitor Rifane
  */
-
-
+@Data
+@EqualsAndHashCode(of = {"email"})
 @Entity
 @Table(name = "USUARIOS")
 @NamedQueries({
     @NamedQuery(name = "AutenticarUsuario",
-               query = "  From Usuario u"
-                     + " Where u.email = :email "
-                     + "   and u.senha = :senha "),
+            query = "  From Usuario u"
+            + " Where u.email = :email "
+            + "   and u.senha = :senha "),
     @NamedQuery(name = "BuscarUsuario",
-               query = "  From Usuario u"
-                     + " Where u.email = :email ")})
+            query = "  From Usuario u"
+            + " Where u.email = :email ")})
 @Views({
-    /**
-     * View de Entrar
-     */
+    //<editor-fold defaultstate="collapsed" desc="Entrar">
     @View(name = "Entrar",
-         title = "Entrar",
-       members = "[#email;#senha;entrar();]",
-    namedQuery = "Select new br.edu.Usuario()",
-         roles = "NOLOGGED"),
-/**
-     * Cadastro de Usuarios
-     */
+            title = "Entrar",
+            members = ""
+            + "[#email;"
+            + " #senha;"
+            + " entrar();"
+            + " [goCadastrarSe(),goEsqueciSenha()]]",
+            namedQuery = "Select new br.edu.Usuario()",
+            roles = "NOLOGGED"),
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="CadastrarSe">
     @View(name = "CadastrarSe",
-         title = "Cadastrar-se",
-       members = "Usuario[#nome;"
-               + "     #email;"
-               + "     #perfis:2;"
-               + "  salvar()]",
-      namedQuery = "Select new br.edu.Usuario()",
-      roles = "NOLOGGED"),
-    /**
-     * View de redefinir senha
-     */
+            title = "Cadastrar-se",
+            members = ""
+            + "Usuario[#nome;"
+            + "        #email;"
+            + "        '':#perfis;"
+            + "        cadastrarSe()]",
+            namedQuery = "Select new br.edu.Usuario()",
+            roles = "NOLOGGED"),
+//</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="RedefinirSenha">
     @View(name = "RedefinirSenha",
-         title = "Redefinir Senha",
-       members = "[#email;redefinirSenha()]",
-    namedQuery = "Select new br.edu.Usuario()",
-         roles = "NOLOGGED"),
-    /**
-     * View de Sair
-     */
+            title = "Redefinir Senha",
+            members = "[#email;redefinirSenha()]",
+            namedQuery = "Select new br.edu.Usuario()",
+            roles = "NOLOGGED"),
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Sair">
     @View(name = "Sair",
-         title = "Sair",
-       members = "[[*email;"
-               + "            *perfis;"
-               + "            [sair()]]]",
-    namedQuery = "From Usuario u Where u = :usuario",
-        params = {@Param(name = "usuario", value = "#{context.currentUser}")},
-         roles = "LOGGED")
-    
+            title = "Sair",
+            members = ""
+            + "[*email;"
+            + " *perfis;"
+            + " sair()]",
+            namedQuery = "From Usuario u Where u = :usuario",
+            params = {
+                @Param(name = "usuario", value = "#{context.currentUser}")},
+            roles = "LOGGED")
+//</editor-fold>
 })
 public class Usuario implements Serializable {
-    
-    public enum Perfil { Professor, Aluno }
- 
+
+    public enum Perfil {
+
+        Professor, Aluno
+    }
+
     @Id
     @GeneratedValue
     private Integer id;
-    
+
     @Column(length = 100)
-    //@NotEmpty(message = "Nome do usuario não informado")
+    @NotEmpty(message = "Nome do usuario não informado")
+    @PropertyDescriptor(displayWidth = 60)
     private String nome;
-    
+
     @Username
-    @Column(length = 100, unique = true)    
-    //@NotEmpty(message = "Informe o e-mail")
+    @Column(length = 100, unique = true)
+    @NotEmpty(message = "Informe o e-mail")
+    @PropertyDescriptor(displayWidth = 25)
     private String email;
-    
-    /*@PropertyDescriptor(displayName = "Sou Professor")
-    private boolean perfilProfessor;
-    
-    @PropertyDescriptor(displayName = "Sou Aluno")
-    private boolean perfilAluno;*/
-    
-    @Column(length = 8)
-    //@NotEmpty(message = "Enter the password")
-    //@Type(type = "entities.security.Password")
-    @PropertyDescriptor(secret = true, displayWidth = 25)    
+
+    @Column(length = 32)
+    @NotEmpty(message = "Enter the password")
+    @Type(type = "entities.security.Password")
+    @PropertyDescriptor(secret = true, displayWidth = 25)
     private String senha;
-    
+
     @UserRoles
     @Enumerated(EnumType.STRING)
-    @ElementCollection(fetch= FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<Perfil> perfis = new ArrayList<Perfil>();
-    
-    public Usuario(){
-        //this.senha = RandomStringUtils.randomAlphanumeric(8);
+
+    //<editor-fold defaultstate="collapsed" desc="Construtores">
+    public Usuario() {
     }
-    
+
     public Usuario(String nome, String email, Perfil... perfis) {
         this.nome = nome;
         this.email = email;
         this.perfis.addAll(Arrays.asList(perfis));
         this.senha = RandomStringUtils.randomAlphanumeric(8);
     }
-    
-     // <editor-fold defaultstate="collapsed" desc="Autorização & Autenticação">    
-    public String entrar() {        
-        
-        List<Usuario> usuarios = Repository.query("AutenticarUsuario", email, senha);
-        if (usuarios.size() == 1) {
-            Context.setCurrentUser(usuarios.get(0));
-        }else {
+    //</editor-fold>
+
+    @ActionDescriptor(preValidate = false)
+    public String entrar() {
+        Usuario usuario = Repository.queryUnique("AutenticarUsuario", email, senha);
+        if (usuario != null) {
+            Context.setCurrentUser(usuario);
+        } else {
             throw new SecurityException("Usuário/Senha incorreto(s)!");
         }
-                    
+
         return "go:home";
     }
-    
-    static public String sair() {
+
+    public String sair() {
         Context.clear();
         return "go:br.edu.Usuario@Entrar";
-    }// </editor-fold>
-    
-    public String cadastrar(){
-        List<Usuario> usuarios = Repository.query("BuscarUsuario", email);        
-        if (usuarios.size() <= 0) {
-            Usuario novoUsuario = new Usuario();
-            novoUsuario.setNome(nome);
-            novoUsuario.setPerfis(perfis);
-            novoUsuario.setSenha(RandomStringUtils.randomAlphanumeric(8));
-            Repository.save(novoUsuario);
-            //enviar email com a sua senha
-        }else {
+    }
+
+    @ActionDescriptor(preValidate = false, value = "Cadastrar-se")
+    public String cadastrarSe() {
+        Usuario usuario = Repository.queryUnique("BuscarUsuario", email);
+        if (usuario != null) {
             throw new SecurityException("Usuário já cadastrado com o e-mail informado!");
         }
-        return "go:br.edu.Usuario@Entrar";
+
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setEmail(email);
+        novoUsuario.setNome(nome);
+        novoUsuario.setPerfis(perfis);
+        novoUsuario.setSenha(RandomStringUtils.randomAlphanumeric(8));
+        Repository.save(novoUsuario);
+
+        //enviar email com a sua senha            
+        GMailBuilder.getInstance().
+                addToMail(email).
+                setSubject("[TurmaOnLine]Cadastro").
+                setMessage("Sua conta: " + novoUsuario + "<br> e senha: " + novoUsuario.getSenha()).
+                sendMail();
+
+        return "Uma senha foi enviada para seu email";
     }
-    
-    public String salvar(){
-        
-        List<Usuario> usuarios = Repository.query("BuscarUsuario", email);        
-        if (usuarios.size() <= 0) {
-            Usuario novoUsuario = new Usuario();
-            novoUsuario.setEmail(email);
-            novoUsuario.setNome(nome);
-            novoUsuario.setPerfis(perfis);
-            novoUsuario.setSenha(RandomStringUtils.randomAlphanumeric(8));
-            Repository.save(novoUsuario);
-            //enviar email com a sua senha
-        }else {
-            throw new SecurityException("Usuário já cadastrado com o e-mail informado!");
-        }
-        return "go:br.edu.Usuario@Entrar";
-    }
-    public String EsqueciSenha(){
-        return "go:br.edu.Usuario@RedefinirSenha";
-    }
-    
-    public String redefinirSenha(){
-        List<Usuario> usuarios = Repository.query("BuscarUsuario", email);
-        if (usuarios.size() == 1) {
-            Usuario usu = usuarios.get(0);
-            usu.setSenha(RandomStringUtils.randomAlphanumeric(8));
-            Repository.save(usu);
+
+    @ActionDescriptor(preValidate = false)
+    public String redefinirSenha() {
+        Usuario usuario = Repository.queryUnique("BuscarUsuario", email);
+        if (usuario != null) {            
+            usuario.setSenha(RandomStringUtils.randomAlphanumeric(8));
+            Repository.save(usuario);
+
             //enviar email com a nova senha
-        }else {
+            GMailBuilder.getInstance().
+                    addToMail(email).
+                    setSubject("[TurmaOnLine]Nova Senha").
+                    setMessage("Sua senha:" + usuario.getSenha()).
+                    sendMail();
+        } else {
             throw new SecurityException("Usuário/Senha incorreto(s)!");
         }
-                    
+
         return "go:home";
     }
-    
-     @Override
+
+    //<editor-fold defaultstate="collapsed" desc="Métodos de Navegação">
+    @ActionDescriptor(componenteType = Types.COMMAND_LINK, value = "Cadastrar-se", immediate = true)
+    public String goCadastrarSe() {
+        return "go:br.edu.Usuario@CadastrarSe";
+    }
+
+    @ActionDescriptor(componenteType = Types.COMMAND_LINK, value = "Esqueci minha senha", immediate = true)
+    public String goEsqueciSenha() {
+        return "go:br.edu.Usuario@RedefinirSenha";
+    }
+    //</editor-fold>
+
+    @Override
     public String toString() {
         return email;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /*public boolean isPerfilProfessor() {
-        return perfilProfessor;
-    }
-
-    public void setPerfilProfessor(boolean perfilProfessor) {
-        this.perfilProfessor = perfilProfessor;
-    }
-
-    public boolean isPerfilAluno() {
-        return perfilAluno;
-    }
-
-    public void setPerfilAluno(boolean perfilAluno) {
-        this.perfilAluno = perfilAluno;
-    }*/
-    
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    
-    public String getSenha() {
-        return senha;
-    }
-
-    public List<Perfil> getPerfis() {
-        return perfis;
-    }
-
-    public void setPerfis(List<Perfil> perfis) {
-        this.perfis = perfis;
-    }
-    
-    
 }
